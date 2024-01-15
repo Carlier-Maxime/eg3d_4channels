89c2b6e0a359edb576e931e9af10bddfca2d95b0
maxime_carlier <28m06c02@gmail.com>
2024-01-15 16:16:06 +0100

    add msg for assertion in copy_params_and_buffers

--- eg3d/torch_utils/misc.py
+++ eg3d/torch_utils/misc.py
@@ -173,7 +173,7 @@ def copy_params_and_buffers(src_module, dst_module, require_all=False):
     assert isinstance(dst_module, torch.nn.Module)
     src_tensors = dict(named_params_and_buffers(src_module))
     for name, tensor in named_params_and_buffers(dst_module):
-        assert (name in src_tensors) or (not require_all)
+        assert (name in src_tensors) or (not require_all), 'the name {} is not in src_tensors'.format(name)
         if name in src_tensors:
             tensor.copy_(src_tensors[name].detach()).requires_grad_(tensor.requires_grad)
 
